72e8e649ef61c312dc8851cac11dd73f8a0a1824
68754807@qq.com <68754807@qq.com>
2019-01-08 08:40:46 +0800

    添加可理解注释

--- helloWord/src/main/java/com/ThreadLock/LockDemo01.java
+++ helloWord/src/main/java/com/ThreadLock/LockDemo01.java
@@ -4,13 +4,17 @@ import com.alice.HelloWord.App;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * Java锁
+ * 悲观锁、乐观锁、排他锁
+ *
+ *
+ */
 public class LockDemo01 {
 
     public static void main(String[] args) {
         App.getHelloWorld();
         CountDownLatch countDownLatch = new CountDownLatch(5);
         System.out.println(countDownLatch.getCount());
-
-        countDownLatch.countDown();
     }
 }
