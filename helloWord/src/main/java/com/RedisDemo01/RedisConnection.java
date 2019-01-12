efb634de140d9b1cf25897b2b60a81773f63ba34
68754807@qq.com <68754807@qq.com>
2019-01-12 18:09:56 +0800

    整理 package

--- helloWord/src/main/java/com/RedisDemo01/RedisConnection.java
+++ helloWord/src/main/java/com/RedisDemo01/RedisConnection.java
@@ -1,4 +1,4 @@
-package com.RedisDemo;
+package com.RedisDemo01;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
--- helloWord/src/main/java/com/RedisDemo01/RedisConnectionTest.java
+++ helloWord/src/main/java/com/RedisDemo01/RedisConnectionTest.java
@@ -1,4 +1,4 @@
-package com.RedisDemo;
+package com.RedisDemo01;
 
 import org.junit.Assert;
 import org.junit.Before;
