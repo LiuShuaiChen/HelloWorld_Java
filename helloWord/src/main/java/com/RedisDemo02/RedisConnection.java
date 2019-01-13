b3387ff032159b60f20007495937b924d1c46576
68754807@qq.com <68754807@qq.com>
2019-01-13 21:28:43 +0800

    redis连接池 后续有时间要写完redis连接池应用

---
+++ helloWord/src/main/java/com/RedisDemo02/RedisConnection.java
@@ -0,0 +1,16 @@
+package com.RedisDemo02;
+
+import org.junit.Test;
+
+//写一个redis连接池
+//2019-1-13 21:27:34
+//暂时还没有写
+public class RedisConnection {
+
+
+    @Test
+    public void test(){
+
+    }
+}
+
