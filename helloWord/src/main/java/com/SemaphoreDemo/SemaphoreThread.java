60ac9b898ad7105d32b2bb16e8235ea43658c319
wangjh <TingYun2o18>
2019-01-14 20:15:11 +0800

    Semaphore是一种基于计数的信号量。它可以设定一个阈值，基于此，多个线程竞争获取许可信号，做自己的申请后归还，超过阈值后，线程申请许可信号将会被阻塞。Semaphore可以用来构建一些对象池，资源池之类的，比如数据库连接池，我们也可以创建计数为1的Semaphore，将其作为一种类似互斥锁的机制，这也叫二元信号量，表示两种互斥状态。它的用法如下： availablePermits函数用来获取当前可用的资源数量 wc.acquire(); //申请资源 wc.release();// 释放资源

--- helloWord/src/main/java/com/SemaphoreDemo/SemaphoreThread.java
+++ helloWord/src/main/java/com/SemaphoreDemo/SemaphoreThread.java
@@ -3,6 +3,13 @@ package com.SemaphoreDemo;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
+/**
+ * Semaphore是一种基于计数的信号量。它可以设定一个阈值，基于此，多个线程竞争获取许可信号，做自己的申请后归还，超过阈值后，线程申请许可信号将会被阻塞。Semaphore可以用来构建一些对象池，资源池之类的，
+ * 比如数据库连接池，我们也可以创建计数为1的Semaphore，将其作为一种类似互斥锁的机制，这也叫二元信号量，表示两种互斥状态。它的用法如下：
+ * availablePermits函数用来获取当前可用的资源数量
+ * wc.acquire(); //申请资源
+ * wc.release();// 释放资源
+ */
 public class SemaphoreThread extends Thread {
 
     private String name;
@@ -31,16 +38,31 @@ public class SemaphoreThread extends Thread {
 
         System.out.println(name + " 申请到资源,剩下的资源为==> " + semaphore.availablePermits());
 
-        try {
-            Thread.sleep(new Random().nextInt(1000));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(new Random().nextInt(100));
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println(name + " 线程 " + Thread.currentThread().getName() +" 资源已经使用完毕!");
         //释放资源
         semaphore.release();
 
 
+    }
+}
+
+
+
+class SemaphoreDemo {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < 100; i++) {
+            SemaphoreThread semaphoreThread = new SemaphoreThread(i + " helloworld", semaphore);
+            semaphoreThread.start();
+        }
     }
 }
