4a2bd0ee702cce694e14d4bc7e36d1be6e5d838c
wangjh <TingYun2o18>
2019-01-14 20:02:35 +0800

    Semaphore是一种基于计数的信号量。它可以设定一个阈值，基于此，多个线程竞争获取许可信号，做自己的申请后归还，超过阈值后，线程申请许可信号将会被阻塞。Semaphore可以用来构建一些对象池，资源池之类的，比如数据库连接池，我们也可以创建计数为1的Semaphore，将其作为一种类似互斥锁的机制，这也叫二元信号量，表示两种互斥状态。它的用法如下： availablePermits函数用来获取当前可用的资源数量 wc.acquire(); //申请资源 wc.release();// 释放资源

--- helloWord/src/main/java/com/SemaphoreDemo/SemaphoreDemo.java
+++ helloWord/src/main/java/com/SemaphoreDemo/SemaphoreDemo.java
@@ -2,6 +2,13 @@ package com.SemaphoreDemo;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * Semaphore是一种基于计数的信号量。它可以设定一个阈值，基于此，多个线程竞争获取许可信号，做自己的申请后归还，超过阈值后，线程申请许可信号将会被阻塞。Semaphore可以用来构建一些对象池，资源池之类的，
+ * 比如数据库连接池，我们也可以创建计数为1的Semaphore，将其作为一种类似互斥锁的机制，这也叫二元信号量，表示两种互斥状态。它的用法如下：
+ * availablePermits函数用来获取当前可用的资源数量
+ * wc.acquire(); //申请资源
+ * wc.release();// 释放资源
+ */
 public class SemaphoreDemo {
 
     public static void main(String[] args) {
