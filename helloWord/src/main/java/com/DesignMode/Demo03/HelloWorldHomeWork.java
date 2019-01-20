74e5381cb71c8282765e7c831b160a373eb9a636
68754807@qq.com <68754807@qq.com>
2019-01-20 21:51:58 +0800

    需求:new 一个对象的时候 利用反射机制获取SQL

---
+++ helloWord/src/main/java/com/DesignMode/Demo03/HelloWorldHomeWork.java
@@ -0,0 +1,12 @@
+package com.DesignMode.Demo03;
+
+/**
+ * 利用封装机制写一个orm框架
+ *
+ * 需求:new 一个对象的时候 利用反射机制获取SQL
+ */
+public class HelloWorldHomeWork {
+    public static void main(String[] args) {
+
+    }
+}
