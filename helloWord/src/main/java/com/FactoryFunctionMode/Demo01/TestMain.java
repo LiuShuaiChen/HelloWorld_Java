5bb65cee1777f31635eee8364b1480893e36fd0e
wangjh <TingYun2o18>
2019-01-22 14:49:22 +0800

    时间工具类LocalDateTime

--- helloWord/src/main/java/com/FactoryFunctionMode/Demo01/TestMain.java
+++ helloWord/src/main/java/com/FactoryFunctionMode/Demo01/TestMain.java
@@ -1,5 +1,8 @@
 package com.FactoryFunctionMode.Demo01;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class TestMain {
     public static void main(String[] args) {
         Car aodi = new AodiFactory().createCar();
@@ -7,6 +10,14 @@ public class TestMain {
 
         Car bmp = new BmpFactory().createCar();
         bmp.run();
+
+        //时间工具类
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Date date = new Date();
+        System.out.println(localDateTime);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
 
     }
 }
