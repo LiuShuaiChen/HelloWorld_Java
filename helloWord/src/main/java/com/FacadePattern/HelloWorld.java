149ec44b0005123dff515179d9c227913576d63c
ShuaichenLau <68754807@qq.com>
2019-02-12 00:50:53 +0800

    父类定义方法 具体实现在子类写

--- helloWord/src/main/java/com/FacadePattern/HelloWorld.java
+++ helloWord/src/main/java/com/FacadePattern/HelloWorld.java
@@ -9,6 +9,15 @@ package com.FacadePattern;
  * 这种模式涉及到一个单一的类，该类提供了客户端请求的简化方法和对现有系统类方法的委托调用。
  *
  */
+
+/**
+ * 但凡设计模式的实现
+ * 一定会有 接口 继承 抽象 多态实现 封装
+ *
+ * 模板方法应用比较多
+ * 通俗来讲就是
+ *  父类定义方法 具体实现在子类写
+ */
 public class HelloWorld {
 
     public static void main(String[] args) {
