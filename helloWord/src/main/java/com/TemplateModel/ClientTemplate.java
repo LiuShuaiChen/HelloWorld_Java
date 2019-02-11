dc80a9f6fc5a2efad60c928b5bddfac6f2fe7f10
ShuaichenLau <68754807@qq.com>
2019-02-11 23:50:00 +0800

    模板方法的应用场景

--- helloWord/src/main/java/com/TemplateModel/ClientTemplate.java
+++ helloWord/src/main/java/com/TemplateModel/ClientTemplate.java
@@ -1,5 +1,9 @@
 package com.TemplateModel;
 
+/**
+ * 什么时候使用模板方法
+ * 实现一些操作时,整体步骤很固定,but其中一小部分容易变,这时候可以使用模板方法模式,将容易变的部分抽象出来,供子类实现.
+ */
 public class ClientTemplate {
 
     public static void main(String[] args) {
