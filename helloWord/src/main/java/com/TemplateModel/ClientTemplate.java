baeff68664c9a53c14f6e87728900fdf9704afbd
ShuaichenLau <68754807@qq.com>
2019-02-12 00:03:07 +0800

    模板方法的应用场景

--- helloWord/src/main/java/com/TemplateModel/ClientTemplate.java
+++ helloWord/src/main/java/com/TemplateModel/ClientTemplate.java
@@ -3,6 +3,14 @@ package com.TemplateModel;
 /**
  * 什么时候使用模板方法
  * 实现一些操作时,整体步骤很固定,but其中一小部分容易变,这时候可以使用模板方法模式,将容易变的部分抽象出来,供子类实现.
+ *
+ * 其实在各个框架中,都会与模板方法模式的影子
+ *
+ * 数据库访问的封装  Junit单元测试 Servlet中关于doGet doPost方法等等调用
+ * Hibernate中模板程序
+ * Spring的JDBCTemplate  HibernateTemplate等等
+ *
+ *
  */
 public class ClientTemplate {
 
