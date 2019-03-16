84620f7cf1438e71faf942786ea3bebf13be12f8
wangjh <TingYun2o18>
2019-03-16 23:56:53 +0800

    显示内存freeMemory totalMemory MaxMemiry

--- helloWord/src/main/java/com/SingleMode/Demo04/abc.java
+++ helloWord/src/main/java/com/SingleMode/Demo04/abc.java
@@ -6,9 +6,13 @@ public class abc {
         String str = "Hello";
         modify(str);
         System.out.println(str);
+        System.out.println(Runtime.getRuntime().freeMemory() / 1024 / 1024 + "M");
+        System.out.println(Runtime.getRuntime().totalMemory() / 1024 / 1024 + "M");
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M");
+
     }
 
-    public static void modify(String str){
+    public static void modify(String str) {
         str += "str";
     }
 }
