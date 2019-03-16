d9751d21e3b5bce76e3e810f9e74e0cd23b24334
wangjh <TingYun2o18>
2019-03-16 23:56:08 +0800

    配置堆内存大小 -Xms20m -Xmx20m -Xmn5m -XX:SurvivorRatio=2 -XX:+PrintGCDetails -XX:+UseSerialGC

---
+++ helloWord/src/main/java/com/alice/HelloWord/heapMemory.java
@@ -0,0 +1,40 @@
+package com.alice.HelloWord;
+
+/**
+ * 配置堆内存大小
+ * -Xms20m -Xmx20m -Xmn10m -XX:SurvivorRatio=2 -XX:+PrintGCDetails -XX:+UseSerialGC
+ * -Xms20m -Xmx20m -Xmn5m -XX:SurvivorRatio=2 -XX:+PrintGCDetails -XX:+UseSerialGC
+ */
+public class heapMemory {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("最大内存:" + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M");
+        System.out.println("可用内存:" + Runtime.getRuntime().freeMemory() / 1024 / 1024 + "M");
+        System.out.println("已经使用内存:" + Runtime.getRuntime().totalMemory() / 1024 / 1024 + "M");
+
+
+        heapMemory heapMemory = new heapMemory();
+        heapMemory.setName("HelloWorld");
+
+        heapMemory = null;
+//        System.gc();
+        //[GC (Allocation Failure) [DefNew: 2560K->886K(3840K), 0.0015883 secs] 2560K->886K(19200K), 0.0016185 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+        //[Full GC (System.gc()) [Tenured: 0K->890K(10240K), 0.0034807 secs] 2690K->890K(17920K), [Metaspace: 3369K->3369K(1056768K)], 0.0035294 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("FULL GC RUNING...");
+    }
+}
