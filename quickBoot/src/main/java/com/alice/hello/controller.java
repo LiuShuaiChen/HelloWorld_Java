175add002c07ac96a210d61236694bfc64b6154c
ShuaichenLau <68754807@qq.com>
2019-03-17 16:16:58 +0800

    JVM性能调优 jmeter配合性能测试

--- quickBoot/src/main/java/com/alice/hello/controller.java
+++ quickBoot/src/main/java/com/alice/hello/controller.java
@@ -1,9 +1,13 @@
 package com.alice.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
@@ -11,11 +15,18 @@ public class controller {
 
     private int count;
 
+    private ArrayList<Student> studentArrayList = new ArrayList<Student>();
+
     @RequestMapping("/")
     String home() {
         int temp = count++;
-        System.out.println(temp);
-        return "Hello World == > " + temp;
+        Student student = new Student();
+        student.setId(temp);
+        student.setName("Alice_" + temp);
+        student.setAddress("Beijing_" + temp);
+        studentArrayList.add(student);
+        System.out.println(student.toString());
+        return "Hello World == > " + studentArrayList.size();
     }
 
     public static void main(String[] args) throws Exception {
