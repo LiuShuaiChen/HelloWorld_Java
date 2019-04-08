a3168ee6fffe48ad2ada6b8024224ce0290d936d
ShuaichenLau <68754807@qq.com>
2019-04-08 22:37:14 +0800

    基于SpringBoot开发一个Restful服务，实现增删改查功能

--- springbootstarterparent/src/main/java/com/pancm/web/UserRestController.java
+++ springbootstarterparent/src/main/java/com/pancm/web/UserRestController.java
@@ -11,38 +11,44 @@ import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
-@RequestMapping(value="/api")
+@RequestMapping(value = "/api")
 public class UserRestController {
 
     @Autowired
     private UserService userService;
 
+    private int count = 9000;
+
     @RequestMapping(value = "/user/add", method = RequestMethod.POST)
-    public boolean adduser(@RequestBody User user){
+    @ResponseBody
+    public boolean adduser(@RequestBody User user) {
         System.out.println("add user ...");
+        count = ++count;
+        user.setId(count);
+        user.setAge(user.getAge() + count);
+        user.setName(user.getName() + count);
+
         return userService.addUser(user);
     }
 
     @RequestMapping(value = "/user/update", method = RequestMethod.POST)
-    public boolean updateUser(@RequestBody User user){
+    @ResponseBody
+    public boolean updateUser(@RequestBody User user) {
         System.out.println("update  user ...");
         return userService.updateUser(user);
     }
 
-    @RequestMapping(value = "/user/del",method = RequestMethod.DELETE)
-    public boolean delUser(@RequestParam(value = "username",required = true) int id){
+    @RequestMapping(value = "/user/del", method = RequestMethod.DELETE)
+    public boolean delUser(@RequestParam(value = "username", required = true) int id) {
         System.out.println("delete user ...");
         return userService.delUser(id);
     }
 
     @RequestMapping(value = "/userAll", method = RequestMethod.GET)
-    public List<User> getAll(){
+    public List<User> getAll() {
         System.out.println("get all user ...");
         return userService.findAll();
     }
-
-
-
 
 
 }
